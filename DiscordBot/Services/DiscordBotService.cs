4d4afdd6fec119a4de9dbd5890c98cb7dbd83579
Eciu <eciek15@gmail.com>
2024-02-19 19:21:05 +0100

    appsettings kurwo

--- DiscordBot/Services/DiscordBotService.cs
+++ DiscordBot/Services/DiscordBotService.cs
@@ -1,31 +1,42 @@
-﻿using DiscordBot;
+﻿using DiscordBot.Models;
 using Microsoft.Extensions.Hosting;
 
 namespace DiscordBot.Services;
 
-public class DiscordBotService(DiscordSocketClient client, InteractionService interactions, IConfiguration config, ILogger<DiscordBotService> logger,
-    InteractionHandler interactionHandler) : BackgroundService
+public class DiscordBotService(
+        DiscordSocketClient client,
+        InteractionService interactions,
+        ILogger<DiscordBotService> logger,
+        InteractionHandler interactionHandler) : BackgroundService
 {
-    Config _config = new Config();
-    protected override Task ExecuteAsync(CancellationToken cancellationToken)
+    protected override Task ExecuteAsync(CancellationToken ct)
     {
+        var sconfig = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json",optional:false,reloadOnChange:true)
+            .Build();
+        BotConfig botConfig = new()
+        { token = sconfig.GetValue<string>("BotConfig:token") ?? throw new Exception("appsettings.json is not specified propely!") };
+
+        if (botConfig.token == null)
+            throw new Exception("BotConfig:token is missing!");
+
         client.Ready += ClientReady;
 
         client.Log += LogAsync;
         interactions.Log += LogAsync;
-        Task task1 = Task.Run(async () => await TimerService.Timer());
+        Task task1 = Task.Run(TimerService.Timer, ct);
 
         return interactionHandler.InitializeAsync()
-            .ContinueWith(t => client.LoginAsync(TokenType.Bot, _config.token), cancellationToken)
-            .ContinueWith(t => client.StartAsync(), cancellationToken);
+            .ContinueWith(t => client.LoginAsync(TokenType.Bot, botConfig.token), ct)
+            .ContinueWith(t => client.StartAsync(), ct);
     }
 
-    public override Task StopAsync(CancellationToken cancellationToken)
+    public override Task StopAsync(CancellationToken ct)
     {
         if (ExecuteTask is null)
             return Task.CompletedTask;
 
-        base.StopAsync(cancellationToken);
+        base.StopAsync(ct);
         return client.StopAsync();
     }
 
