236051846fca26c6c4c2ed5ce5682c7390e6d049
Hepiix <133647633+Hepiix@users.noreply.github.com>
2024-02-20 20:33:37 +0100

    fix

--- DiscordBot/Modules/ConfigBotModule.cs
+++ DiscordBot/Modules/ConfigBotModule.cs
@@ -1,6 +1,7 @@
 ﻿using DiscordBot.Services;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 namespace DiscordBot.Modules
 {
     
+
     public class ConfigBotModule : InteractionModuleBase<SocketInteractionContext>
     {
         public ConfigBotService ConfigBotService { get; set; }
@@ -16,11 +18,14 @@ namespace DiscordBot.Modules
         [SlashCommand("config", "Konfiguracja funkcji bota")]
         public async Task ConfigBot()
         {
-
             var builder = ConfigBotService.Build(Context);
             await RespondAsync("", ephemeral: true, components: builder.Build());
+        }
 
-            
+        // Ta funkcja jest tutaj tylko po to żeby nie wywalało błędu xD
+        [ComponentInteraction("configMenu")]
+        public async Task ConfigMenu()
+        {
         }
     }
 }
