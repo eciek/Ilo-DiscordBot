0ada53359d578c72fb96152851f0c75c42aa9e4f
Eciu <eciek15@gmail.com>
2024-03-04 11:59:18 +0100

    uncomment

--- DiscordBot/Modules/AnimeFeed/AnimeFeedModule.cs
+++ DiscordBot/Modules/AnimeFeed/AnimeFeedModule.cs
@@ -26,59 +26,59 @@ public class AnimeFeedModule : InteractionModuleBase<SocketInteractionContext>
         _timerService.RegisterJob(animeFeedJob);
     }
 
-    //[SlashCommand("anime-dodaj", "Dodaj do listy wołania na nowy odcinek anime")]
-    //public async Task AnimeAdd([Name("Nazwa Anime")] [MinLength(4)]string anime)
-    //{
-    //    Anime foundAnime;
-    //    try
-    //    {
-    //        foundAnime = await _animeFeedService.MatchAnime(anime);
-    //    }
-    //    catch (Exception ex) 
-    //    {
-    //        await RespondAsync(ex.Message, ephemeral: true);
-    //        return;
-    //    }
+    [SlashCommand("anime-dodaj", "Dodaj do listy wołania na nowy odcinek anime")]
+    public async Task AnimeAdd([Name("Nazwa Anime")] [MinLength(4)]string anime)
+    {
+        Anime foundAnime;
+        try
+        {
+            foundAnime = await _animeFeedService.MatchAnime(anime);
+        }
+        catch (Exception ex) 
+        {
+            await RespondAsync(ex.Message, ephemeral: true);
+            return;
+        }
 
-    //    _animeListService.AddAnimeSubscriber(Context.Guild.Id, Context.User.Id, foundAnime);
+        _animeListService.AddAnimeSubscriber(Context.Guild.Id, Context.User.Id, foundAnime);
 
-    //    await RespondAsync($"Dodałam Cię do listy {foundAnime.Name}!",ephemeral: true);
-    //}
+        await RespondAsync($"Dodałam Cię do listy {foundAnime.Name}!",ephemeral: true);
+    }
 
-    //[Discord.Commands.RequireUserPermission(GuildPermission.Administrator)]
-    //[SlashCommand("anime-usuń", "Usuń z listy wołania na nowy odcinek anime")]
-    //public async Task AnimeDelete([Optional] [Name("Nazwa Anime")] [MinLength(4)] string anime)
-    //{
-    //    Anime foundAnime;
-    //    try
-    //    {
-    //        foundAnime = await _animeFeedService.MatchAnime(anime);
-    //    }
-    //    catch (Exception)
-    //    {
-    //        try
-    //        {
-    //            var userAnimeList = _animeListService.GetUserAnimeList(Context.Guild.Id, Context.User.Id);
+    [Discord.Commands.RequireUserPermission(GuildPermission.Administrator)]
+    [SlashCommand("anime-usuń", "Usuń z listy wołania na nowy odcinek anime")]
+    public async Task AnimeDelete([Optional] [Name("Nazwa Anime")] [MinLength(4)] string anime)
+    {
+        Anime foundAnime;
+        try
+        {
+            foundAnime = await _animeFeedService.MatchAnime(anime);
+        }
+        catch (Exception)
+        {
+            try
+            {
+                var userAnimeList = _animeListService.GetUserAnimeList(Context.Guild.Id, Context.User.Id);
 
-    //            string errmsg = "O to anime które obserwujesz: \n"
-    //                + string.Join(",\n", userAnimeList)
-    //                + ".\n\n" + "Wybierz właściwą nazwę!";
-    //            await RespondAsync(errmsg, ephemeral: true);
-    //            return;
-    //        }
-    //        // This exception covers GetUserAnimeList messages
-    //        catch (Exception ex)
-    //        {
-    //            await RespondAsync(ex.Message, ephemeral: true);
-    //            return;
-    //        }
-    //    }
+                string errmsg = "O to anime które obserwujesz: \n"
+                    + string.Join(",\n", userAnimeList)
+                    + ".\n\n" + "Wybierz właściwą nazwę!";
+                await RespondAsync(errmsg, ephemeral: true);
+                return;
+            }
+            // This exception covers GetUserAnimeList messages
+            catch (Exception ex)
+            {
+                await RespondAsync(ex.Message, ephemeral: true);
+                return;
+            }
+        }
 
-    //    _animeListService.RemoveAnimeSubscriber(Context.Guild.Id, Context.User.Id, foundAnime);
-    //    await RespondAsync($"Już nie obserwujesz {foundAnime.Name}. \n" +
-    //        $"Co Ci się nie spodobało w tym anime?",ephemeral:true);
-    //    return;
-    //}
+        _animeListService.RemoveAnimeSubscriber(Context.Guild.Id, Context.User.Id, foundAnime);
+        await RespondAsync($"Już nie obserwujesz {foundAnime.Name}. \n" +
+            $"Co Ci się nie spodobało w tym anime?",ephemeral:true);
+        return;
+    }
 
     public async void Update()
     {
